2d2a4a5a37d64157407b8e378ade78c60e461f29
j5delabr <j5delabr@enib.fr>
2018-11-09 05:36:25 +0100

    Affichage de la page d'accueil avec la base de donnée, navigation des pages à vérifier

--- src/components/DemandeCard.js
+++ src/components/DemandeCard.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
-import { Card, Col, Row, Tag, Icon, Divider } from 'antd';
-const { Meta } = Card;
+import { Icon } from 'antd';
+import User from "../classes/User"
 class DemandeCard extends Component {
 
-  render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title : this.props.args.title,
+			author : this.props.args.author,
+			description : this.props.args.description,
+			category : this.props.args.category,
+			id : this.props.args.key
+	};
+	}
+
+   render() {
+	let author = this.state.author 
+	let iconType ;
+	let userInfo = author.getName()+" " + author.getSurname() + " | " + author.getNickname()
+	  console.log(this.state.category)
+	if (this.state.category === "CarPooling" )
+	   {
+		   iconType = "car"
+
+	  	 }	
+
+	   if (this.state.category === "Study" )
+	   {
+
+
+		   iconType = "book"
+
+
+		}
+
+	   if  (this.state.category === "Sharing") 
+	   {
+		   iconType = "interation"
+
+		}
+	  console.log(iconType)
+
+
 
     return (
       <div style={{ overflow: 'hidden', width: '250px' }}>
-        <div style={{ marginTop: '40px', width: '254px', height: '152px', marginLeft: '10px', marginRight: '10px', backgroundColor: '#FFF', overflowY: 'scroll', textOverflow: 'ellipsis' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} >
-            <div style={{ overflow: 'hidden', wordWrap: 'break-word', whiteSpace: 'normal', marginLeft: '5px', fontWeight: 'bold' }} >This is  a long long long title test</div>
-            <div style={{ marginRight: '5px' }}> <Icon type="car" theme="twoTone" /></div>
+        <div style={{ marginTop: '40px', width: '250px', height: '120px', backgroundColor: '#FFF', overflowY: 'scroll', textOverflow: 'ellipsis' }}>
+          <div style={{ display: 'flex', width: '100%' }} >
+            	<div style={{  flexGrow : "1" ,overflow: 'hidden', wordWrap: 'break-word', whiteSpace: 'normal', fontWeight: 'bold' , textAlign :'center' }} >
+	    	{this.state.title}
+	    	</div>
+            	<div> 
+	    		<Icon type={iconType} theme="twoTone" />
+	    	</div>
           </div>
-          <div style={{ fontSize: '8px', color: '#1890ff', overflow: 'hidden', textAlign: 'center', marginTop: '9px' }}>Rodrigo Le Phase | Princeu</div>
+          <div style={{ fontSize: '11px', color: '#1890ff', overflow: 'hidden', textAlign: 'center', marginTop: '9px' }}>
+	    {userInfo}
+	  </div>
           <div style={{ marginTop: '10px', marginLeft: '10px', marginRight: '10px' }} >
-            <p style={{ fontSize: '11px', textAlign: 'justify' }}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.This is  ad
-          </p>
+            	<p style={{ fontSize: '12px', textAlign: 'justify' }}>
+	    		{this.state.description}
+          	</p>
           </div>
         </div>
       </div>

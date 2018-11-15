c0c549cab55bfed57c14ee62c91e07ae699ee67e
d5levequ <d5levequ@enib.fr>
2018-11-15 14:39:17 +0100

    modification de app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import Router from './router'
-import './App.css';
+import Router from './router/router'
+import './App.css'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isConnect: false
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Router/>
+        <Router isConnect={this.state.isConnect}/>
       </div>
     );
   }

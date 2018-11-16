fd405d6fea7322e4eb49a1ae99c85b3d84b98f7d
d5levequ <d5levequ@enib.fr>
2018-11-16 20:19:37 +0100

    ajout d'un systeme de route dynamique

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route as PublicRoute } from 'react-router-dom'
 import PrivateRoute from './privateRoute'
+import * as routes from './routes'
 
 /**
  * Importation des différentes pages de notre application
  */
-import HomePage from '../pages/homePage'
-import RequestCreatorPage from '../pages/requestCreatorPage'
-import RequestPage from '../pages/requestPage'
-import LoginPage from '../pages/loginPage'
-import NotFoundPage from '../pages/notFoundPage'
+import HomePage             from '../pages/homePage'
+import RequestCreatorPage   from '../pages/requestCreatorPage'
+import RequestPage          from '../pages/requestPage'
+import SignInPage           from '../pages/signInPage'
+import SignUpPage           from '../pages/signUpPage'
+import NotFoundPage         from '../pages/notFoundPage'
 
 /**
  * Définition de l'ensemble des différentes routes de notre application
@@ -18,13 +20,14 @@ import NotFoundPage from '../pages/notFoundPage'
 const Router = (props) => (
     <BrowserRouter>
         <Switch>
-            <PublicRoute exact path='/'                     component={HomePage}    />
-            <PublicRoute exact path='/creer-une-demande'    component={RequestCreatorPage} />
-            <PublicRoute exact path='/login'                component={LoginPage}   />
-            <PublicRoute exact path='/logout' />
-            <PublicRoute exact path='/demande/:id'  component={RequestPage}  />
-            <PrivateRoute isConnect={props.isConnect} path='/demande/:id/edit' to='/login' component={RequestPage}  />
-            <PublicRoute component={NotFoundPage} />
+            <PublicRoute  component={HomePage}           exact path={routes.HOME}           />
+            <PublicRoute  component={RequestCreatorPage} exact path={routes.CREATE_DEMAND}  />
+            <PublicRoute  component={SignInPage}         exact path={routes.SIGN_IN}        />
+            <PublicRoute  component={SignUpPage}         exact path={routes.SIGN_UP}        />
+            <PublicRoute                                 exact path={routes.LOGOUT}         />
+            <PublicRoute  component={RequestPage}        exact path={routes.DISPLAY_DEMAND} />
+            <PrivateRoute component={RequestPage}              path={routes.EDIT_DEMAND}    to={routes.SIGN_IN} isConnect={props.isConnect} />
+            <PublicRoute  component={NotFoundPage} />
         </Switch>
     </BrowserRouter>
 );

8f14ff72ce4ea0f26fc546bf6ea9bc38cba2f496
Jarod <j5delabr@enib.fr>
2018-11-17 13:57:59 +0000

    Modified the centered elements

--- src/App.js
+++ src/App.js
@@ -10,9 +10,24 @@ constructor(props) { // constructuer de Top
 		
 		super(props); 
 		this.ismenu = true;
-		this.state = {visible : true}
+		this.state = {visible : true , width: window.innerWidth,}
 				}
 
+
+
+componentWillMount() {
+  window.addEventListener('resize', this.handleWindowSizeChange);
+}
+
+// make sure to remove the listener
+// when the component is not mounted anymore
+componentWillUnmount() {
+  window.removeEventListener('resize', this.handleWindowSizeChange);
+}
+
+handleWindowSizeChange = () => {
+  this.setState({ width: window.innerWidth });
+};
 
 		togleSideBar = () => {
 			if (this.state.visible=== true) {
@@ -31,13 +46,22 @@ constructor(props) { // constructuer de Top
 		}
   render() {
 
+		const { width } = this.state;
+  		const isMobile = width <= 500;
+
 		let cards ;
 		let space ;
 		let pagination;
-		cards = <Row><Demande_card/> <Demande_card/> <Demande_card/> </Row>;
+	  	let centeredCard;
+	  	centeredCard = <div style={{display:'flex' , justifyContent:'center' , alignItems:'center'}}>  <Demande_card/> </div>;
 		space = <Row><br/></Row>;
-		pagination = <Row><Col offset={10}> <Pagination defaultCurrent={1} total={50} />  </Col></Row>;				
-
+	  	if (isMobile){
+		cards = <div>{space}<Row> {centeredCard} </Row>{space}<Row> {centeredCard} </Row>{space}<Row>  </Row>{space}<Row> {centeredCard} </Row>{space}<Row> {centeredCard}  </Row></div>;
+		}
+	  	else  {
+		cards =<div>{space}<Row><Demande_card/> <Demande_card/> <Demande_card/> </Row>{space}<Row><Demande_card/> <Demande_card/> <Demande_card/> </Row>{space}<Row><Demande_card/> <Demande_card/> <Demande_card/> </Row></div>;
+		}
+		pagination = <Row> <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>     <Pagination defaultCurrent={1} total={50} />  </div> </Row>;				
     return (
       <div className="App">
 	    <Layout style={{height:"100vh"}}> 
@@ -45,11 +69,6 @@ constructor(props) { // constructuer de Top
 	    	<Layout>
         		<Top ismenu={this.ismenu} togleSideBar ={this.togleSideBar}/>
 						<Content>
-								{space}
-								{cards}
-								{space}
-								{cards}
-								{space}
 								{cards}
 								{space}
 								{pagination}
--- src/Demande_card.js
+++ src/Demande_card.js
@@ -11,9 +11,10 @@ class Demande_card extends Component {
 		<div style={{textAlign :'center' , fontSize : '15'}}> 
         <Card
     style={{ width: 275 , height :150 , fontSize : '8px' }}
+	    extra = {<Tag color = "blue" style={{fontSize : '8px'}}>Covoiturage</Tag>}
+        title="Trajet Paris / Brest"
       >
       <Meta
-        title="Trajet Paris / Brest"
         description = "Jean Pierre | Poney"
         />
         <br/>
@@ -25,10 +26,6 @@ class Demande_card extends Component {
             Description ...
         </Row>
         <Row>
-            Description ...
-        </Row>
-        <Row>
-            <Col offset ={20} span ={4} > <Tag color = "blue" style={{fontSize : '8px'}}>Covoiturage</Tag></Col>
         </Row>
            </div>
       

ac72dbf31c06b9315ccadb755e0ed8bf09fc3b2b
d5levequ <d5levequ@enib.fr>
2018-11-17 15:05:22 +0100

    Ajout page inscription + formulaire

---
+++ src/pages/signUpPage.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import SignUpForm from '../components/signUpForm'
+import {Layout,  Row, Col, Card } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+class SignUpPage extends Component {
+    render() {
+        return (
+
+          <Layout>
+            <Header>Header</Header>
+          <Layout style = {{background: '#ffffff'}}>
+              <Content style={{paddingTop: '60px'}}>
+                <Row>
+                  <Col 
+                    xs={{offset:1, span:22}} 
+                    sm={{offset:6 ,span:12}} 
+                    md={{offset:7, span:10}} 
+                    lg={{offset:8, span:8}} 
+                    xl={{offset:9, span:6}}
+                  >
+                    <Card title="Bonjour" style={{ width: 300 }} >
+                      <SignUpForm routerHistory={this.props.history} />
+                    </Card>
+                  </Col>
+                </Row> 
+              </Content>
+          </Layout>
+          </Layout>
+        )
+    }
+}
+
+export default SignUpPage;

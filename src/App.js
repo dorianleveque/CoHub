5e60b58a1ee6c63656fdca7d2dd67eff268bd9cf
d5levequ <d5levequ@enib.fr>
2018-11-25 16:38:07 +0100

    Bug session résolu (attendre que notre socket firebase soit bien activé)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import Router from './router/router'
+import { SessionStore } from './stores/session-store'
+import Session from './classes/Session';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      isConnect: false
+      displayApp: false,
+      session: new Session()
     }
   }
 
+  componentDidMount() {
+    // Lorsque la session est prête, on affiche notre application
+    this.state.session.whenSessionReady(() => {
+      console.log('fait')
+      this.setState({ displayApp: true })
+    })
+  }
+
+  /**
+   * La fonction render de App, affiche le router
+   * lorsque la session est prête, sinon une page blanche
+   */
   render() {
+    let router = (this.state.displayApp) 
+                    ? <Router authUser={this.state.session}/>
+                    : null
 		  return (
       <div className="App">
-        <Router isConnect={this.state.isConnect}/>
+        <SessionStore.Provider value={this.state.session} >
+          {router}
+        </SessionStore.Provider>
       </div>
     );
   }

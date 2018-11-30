7a299491eb4025961af2cd6ee9edd39beb4dc48e
d5levequ <d5levequ@enib.fr>
2018-11-30 18:19:07 +0100

    Ajout de la page pour reset son mot de passe

---
+++ src/pages/ResetPasswordPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Top from '../components/Top'
+import { Layout, Row, Card } from 'antd';
+import ResetPasswordForm from '../components/forms/ResetPasswordForm'
+import cohub from '../images/CoHub.png'
+
+const { Content, Footer } = Layout;
+const { Meta } = Card;
+
+const style = {
+  layout: {
+    height: '100vh'
+  },
+  content: {
+    height: '100%'
+  },
+  card: {
+    minWidth: '320px',
+    maxWidth: '400px'
+  },
+  frame: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: '20px'
+  },
+  img: {
+    width: '90px',
+    height: 'auto',
+    position: 'relative'
+  }
+}
+
+class ResetPasswordPage extends Component {
+  render() {
+    return (
+      <Layout style={ style.layout } >
+        <Top/>
+        <Content>
+          <Row type='flex' align='middle' justify='center' style={ style.content } >
+            <Card 
+              cover={
+                <div style={ style.frame } >
+                  <img alt="cohub" src={ cohub } style={ style.img } />
+                </div> }
+              style={ style.card } 
+            >
+              <Meta title="Ré-initialisation mot de passe" />
+              <br/>
+              <ResetPasswordForm routerHistory={this.props.history} />
+            </Card>
+          </Row>
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    )
+  }
+}
+
+export default ResetPasswordPage;

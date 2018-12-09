fbc96df33ff1718c0ccc1ba3556515b8fd1710aa
d5levequ <d5levequ@enib.fr>
2018-12-09 01:52:35 +0100

    modification de la route request page

--- src/router/router.js
+++ src/router/router.js
@@ -43,7 +43,7 @@ class Router extends Component {
                 <PublicRoute    component={SignInPage         } exact path={SIGN_IN}        />
                 <PublicRoute    component={SignUpPage         } exact path={SIGN_UP}        />
                 <PublicRoute    component={RequestPage        } exact path={DISPLAY_DEMAND} />
-                <PrivateRoute   component={RequestPage        }       path={EDIT_DEMAND}    redirectTo={SIGN_IN} />
+                <PrivateRoute   component={RequestPage        } exact path={EDIT_DEMAND}    redirectTo={SIGN_IN} />
                 <ProtectedRoute component={ResetPasswordPage  }       path={ACCOUNT}        withSearchParams={{ mode: 'resetPassword' }} />
                 
                 <PublicRoute    component={TestPageDodo       } exact path={PAGE_TEST_DODO} />

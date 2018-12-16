c1694c74d7c9b53c3c9d62ec47656b56ca849a74
j5delabr <j5delabr@enib.fr>
2018-12-16 20:50:47 +0100

    Added the submenu titles, change the  display order of tickets

--- src/components/SiderMenu.js
+++ src/components/SiderMenu.js
@@ -4,6 +4,7 @@ import { Layout } from 'antd';
 const { Sider } = Layout;
 
 const Search = Input.Search;
+const MenuItemGroup = Menu.ItemGroup
 
 class SiderMenu extends Component {
   constructor(props) { // constructuer de Top
@@ -63,9 +64,10 @@ class SiderMenu extends Component {
     return (
 
       <Sider style={{ background: '#fff' }} collapsed={this.props.visible} collapsedWidth={0} >
-        <div style={{ verticalAlign: 'middle', textAlign: 'center', lineHeight: '70px', background: '#1890ff', color: '#fff', fontSize: '20px', height: '70px' }}> Categorie </div>
+        <div style={{ verticalAlign: 'middle', textAlign: 'center', lineHeight: '70px', background: '#1890ff', color: '#fff', fontSize: '20px', height: '70px' }}> Filtrage </div>
         <div>
           <Menu mode="inline" defaultSelectedKeys={['1']} onClick={this.changeCategory} style={{ height: '100%', background: '#fff' }}>
+	    <MenuItemGroup title = "Catégorie">    
             <Menu.Item key="1">
               <span>Tout</span>
             </Menu.Item>
@@ -80,11 +82,17 @@ class SiderMenu extends Component {
             <Menu.Item key="4">
               <span>Prêt</span>
             </Menu.Item>
+            </MenuItemGroup>
+
+	    <MenuItemGroup title = "Recherche">    
 		<Menu.Item key="5">
 	<Search
             onSearch={value => this.searchItems(value)}
             style={{ width: '130px'  }}/>
             </Menu.Item>
+            </MenuItemGroup>
+
+
           </Menu>
         </div>
       </Sider>

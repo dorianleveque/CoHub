52fad4ec4a79d555d796252d2d2856488b872e63
=e5even <=e5even@enib.fr>
2018-12-16 18:34:52 +0100

    Update RequestPage.js
    Mise à jour consulation

--- src/pages/RequestPage.js
+++ src/pages/RequestPage.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import Top from '../components/Top'
+import TicketListControleur from '../classes/TicketListControleur.js'
+import { Ticket } from '../classes/Ticket.js'
+import { TicketStudy } from '../classes/TicketStudy.js'
+import { TicketSharing } from '../classes/TicketSharing.js'
+import { TicketCarPooling } from '../classes/TicketCarPooling.js'
 import { Input,Divider,Layout,Row, Col, Mention, Button, Cascader} from 'antd';
+
 const { Header, Footer, Sider, Content } = Layout;
 const { toString } = Mention;
-//function onChange(editorState) {console.log(toString(editorState));}
 const Search = Input.Search;
 const { TextArea } = Input
 const options = [{value: 'Tutorat',label: 'Tutorat'},{value: 'Objet',label: 'Pret d objet'},{value: 'Covoiturage',label: 'Covoiturage'}];
 function onChange(value) {console.log(value);}
 
-function leaule()
+/*
+function ticketRecup() // Recupere le ticket en fonction de l'id envoyé dans l'url
 	{
-		//var hu = document.getElementById("semestre").value;
-		//alert(hu);
-		console.log("hey !");
+		//const IdTicket= this.props.match.params.id ; //
+		var IdTicket="-LTdiqMjM6UUvG2TO2ws";
+		console.log(IdTicket);
+		var tlc= new TicketListControleur;
+		var t = null;
+		t=tlc.retriveTicket(IdTicket);
+		console.log(t);
+		return t;
 	}
+*/	
+	var t=null;
 
 class Demande_Consultation  extends Component {
 
@@ -32,12 +44,43 @@ class Demande_Consultation  extends Component {
 				{value: 'Objet',label: "Prêt d'objet"},
 				{value: 'Covoiturage',label: 'Covoiturage'}
 			],
+			
+			ticGlobalInfo: {titre: null, categorie: null, description: null},
+			ticSharingInfo: {objet: null},
+			ticStudyInfo: {matiere:null, prof:null, theme:null, semestre: null},
+			ticCarpoolingInfo: {arrivee:null, depart:null, arriveeTime:null, departTime:null, places:null},
+			
+			
 		}
-	}
-	componentDidMount() {
 		
-	}
+		
+		this.ticketRecup().then((value)=> 
+		{
+			let cat = value.getCategory();
+			console.log(cat);
+			
+			switch(String(cat)) {
+			case "Study": 
+				this.setState({isSharing: false, isStudy: true, isCarPooling: false, Categorie: "Tutorat", ticGlobalInfo:{titre: value.getTitle(), description:value.getDescription()},ticStudyInfo: {matiere:value.getSubject(), prof:value.getTeacher(), theme:value.getTheme(), semestre:value.getSemester()}, })
+				break;
+			case "Sharing":
+				this.setState({isSharing: true, isStudy: false, isCarPooling: false, Categorie: "Objet", ticGlobalInfo:{titre: value.getTitle(), description:value.getDescription()}, ticSharingInfo: {objet: value.getItem()},})
+				console.log(value.getDescription());
+				console.log(this.state.ticGlobalInfo.description);
+				break;
+			case "CarPooling":
+				this.setState({isSharing: false, isStudy: false, isCarPooling: true, Categorie: "Covoiturage", ticGlobalInfo:{titre: value.getTitle(), description:value.getDescription()},ticCarpoolingInfo: {arrivee:value.getArrivalLocation(), depart:value.getDeparturLocation(), arriveeTime:value.getArrivalTime(), departTime:value.getDeparturTime(), places:value.getPlaces()} })
+				break;
+			default:
+				this.setState({isSharing: false, isStudy: false, isCarPooling: false, Categorie: "" })
+				alert("Pas de Categorie")
+		}
+		
+		})
 	
+	}
+
+	/*
 	onCascaderChange(value) {
 		switch(String(value)) {
 			case "Tutorat": 
@@ -54,6 +97,23 @@ class Demande_Consultation  extends Component {
 				alert("Pas de Categorie")
 		}
 	}
+	*/
+	
+	async ticketRecup() // Recupere le ticket en fonction de l'id envoyé dans l'url
+	{
+		//const IdTicket= this.props.match.params.id ; //
+		var IdTicket="-LTdiqMjM6UUvG2TO2ws";
+		console.log(IdTicket);
+		var tlc= new TicketListControleur;
+		t = await tlc.retriveTicket(IdTicket).then((value) => {
+			return value
+		});
+		return t
+		
+			
+	}
+	
+	
 	
 	
   render() {
@@ -66,32 +126,39 @@ class Demande_Consultation  extends Component {
 	}
 	
 	
-	console.log(this.state.Categorie);
+	 // Creation du ticket contenant les infos
+
+	 // Mise a jour des variables état pour l'affichage conditionnel 
+	//let cat = t.getCategory;
+
+	//console.log(cat);
+	
+	
 	let component;
 	
 	if(this.state.isStudy)
 	{
 		component = <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: 'space-evenly' }} >
-			{h3PlusInput('#7F7F7F',"Matiere","Maths",'#42A6FB',"matiere", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Professeur","Mr le prof",'#42A6FB',"prof", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Thème","Le theme",'#42A6FB',"theme", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Semestre","SX",'#42A6FB',"semestre", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Matiere",this.state.ticStudyInfo.matiere,'#42A6FB',"matiere", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Professeur",this.state.ticStudyInfo.prof,'#42A6FB',"prof", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Thème",this.state.ticStudyInfo.theme,'#42A6FB',"theme", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Semestre",this.state.ticStudyInfo.semestre,'#42A6FB',"semestre", this.state.isViewMode)}
 		</div>
 	}
 	else if(this.state.isSharing)
 	{
 		component = <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: 'space-evenly' }} >
-			{h3PlusInput('#7F7F7F',"Objet","Tournevis",'#42A6FB',"objet", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Objet",this.state.ticSharingInfo.objet,'#42A6FB',"objet", this.state.isViewMode)}
 		</div>
 	}
 	else if(this.state.isCarPooling)
 	{
 		component = <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: 'space-evenly' }} >
-			{h3PlusInput('#7F7F7F',"Départ","Brest",'#42A6FB',"depart", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Arrivée","Marseilles",'#42A6FB',"arrivee", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Places","3",'#42A6FB',"places", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Date de départ","11/06/2018",'#42A6FB',"depart_date", this.state.isViewMode)}
-			{h3PlusInput('#7F7F7F',"Date d'arrivée","12/06/2018",'#42A6FB',"arrivee_date", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Départ",this.state.ticCarpoolingInfo.depart,'#42A6FB',"depart", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Arrivée",this.state.ticCarpoolingInfo.arrivee,'#42A6FB',"arrivee", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Places",this.state.ticCarpoolingInfo.places,'#42A6FB',"places", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Date de départ",this.state.ticCarpoolingInfo.departTime,'#42A6FB',"depart_date", this.state.isViewMode)}
+			{h3PlusInput('#7F7F7F',"Date d'arrivée",this.state.ticCarpoolingInfo.arriveeTime,'#42A6FB',"arrivee_date", this.state.isViewMode)}
 		</div>
 	}
 	else
@@ -101,10 +168,7 @@ class Demande_Consultation  extends Component {
 	
 	
 	
-	// IMPORTANT
-	const IdTicket= this.props.match.params ;
-	console.log(IdTicket);
-	// faire un set state par la suite
+	
 	
     return (
 	
@@ -118,11 +182,11 @@ class Demande_Consultation  extends Component {
 				</Row>
 				<Row type="flex" justify="space-around" style={{ textAlign:'center'}}>
 					<Col span={16} >
-						{h3PlusInput('#7F7F7F',"TITRE","Titre du ticket",'#42A6FB','title', this.state.isViewMode, {width: '100%'} )}
+						{h3PlusInput('#7F7F7F',"TITRE",this.state.ticGlobalInfo.titre,'#42A6FB','title', this.state.isViewMode, {width: '100%'} )}
 					</Col>
 					<Col span={6}>
 						<h3 style= {{color:'#7F7F7F'}}>Catégorie</h3>
-						<Cascader options={this.state.categorieOptions} onChange={this.onCascaderChange} value={[this.state.Categorie]} disabled={this.state.isViewMode} style= {{color:'#42A6FB'}}/>
+						<Cascader options={this.state.categorieOptions}  value={[this.state.Categorie]} disabled={this.state.isViewMode} style= {{color:'#42A6FB'}}/>
 					</Col>
 				</Row>
 				{component}
@@ -130,7 +194,8 @@ class Demande_Consultation  extends Component {
 					<div style={{left:0, width:100, background: '#2699FB',textAlign:'center',padding:10}}>
 						<h5 style= {{color:'#fff'}}>DESCRIPTION</h5>
 					</div>
-						<TextArea  />
+						<TextArea  value={this.state.isViewMode ? this.state.ticGlobalInfo.description : ""} />
+						
 				</Row>
 				<Row>
 					<Col>
